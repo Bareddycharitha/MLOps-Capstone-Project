43a943453398ee21bd8c0f6d0a3228f152856e9c
Bareddycharitha <charithabareddy@gmail.com>
2026-03-08 13:43:22 +0530

    CICD 13

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -1,6 +1,7 @@
 # register model
 
 import json
+from typing import Set
 import mlflow
 import logging
 from src.logger import logging
@@ -13,26 +14,26 @@ warnings.filterwarnings("ignore")
 
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
-# Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+#Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+   raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "Bareddycharitha"
-# repo_name = "MLOps-Capstone-Project"
-#  #Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+dagshub_url = "https://dagshub.com"
+repo_owner = "Bareddycharitha"
+repo_name = "MLOps-Capstone-Project"
+ #Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/Bareddycharitha/MLOps-Capstone-Project.mlflow')
-dagshub.init(repo_owner='Bareddycharitha', repo_name='MLOps-Capstone-Project', mlflow=True)
+#mlflow.set_tracking_uri('https://dagshub.com/Bareddycharitha/MLOps-Capstone-Project.mlflow')
+#dagshub.init(repo_owner='Bareddycharitha', repo_name='MLOps-Capstone-Project', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
